7f20904dc0c48ad4b304c3cac8cab306b571c371
NicoConde <nicoconde99@hotmail.com>
2020-06-03 18:03:08 -0300

    Agrego PROC y cambios en tipo de datos PK
    Se crean algunos Procedures
    Se modifican algunos tipos de datos de PK con autonuméricos
    Se agregan "NOT NULL"  las PK

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -63,7 +63,7 @@ GO
 /* CREO TABLAS */
 
 CREATE TABLE LOS_BORBOTONES.CLIENTE (
-		cliente_id INT IDENTITY(1,1),
+		cliente_id INT IDENTITY(1,1) NOT NULL,
 		cliente_apellido nvarchar(255),
 		cliente_nombre nvarchar(255),
 		cliente_dni decimal (18,0),
@@ -74,77 +74,77 @@ CREATE TABLE LOS_BORBOTONES.CLIENTE (
 
 CREATE TABLE LOS_BORBOTONES.SUCURSAL (
 
-		sucursal_id decimal(18,0),
+		sucursal_id INT IDENTITY(2,1) NOT NULL,
 		sucursal_direccion nvarchar(255),
 		sucursal_mail nvarchar(255),
 		sucursal_telefono decimal(18,0)
 )
 
 CREATE TABLE LOS_BORBOTONES.AEROLINEA (
-		aerolinea_codigo nvarchar(255),
+		aerolinea_codigo INT IDENTITY(1,1) NOT NULL,
 		aerolinea_razon_social nvarchar(255)
 )
 
 CREATE TABLE LOS_BORBOTONES.AVION (
-		avion_id nvarchar(50),
+		avion_id nvarchar(50) NOT NULL,
 		avion_modelo nvarchar(50)
 )
 
 CREATE TABLE LOS_BORBOTONES.VUELO (
-		vuelo_codigo decimal(18,0),
+		vuelo_codigo decimal(19,0) NOT NULL,
 		vuelo_fecha_salida datetime2(3),
 		vuelo_fecha_llegada datetime2(3),
 		vuelo_avion_id nvarchar(50),
 		vuelo_ruta_aerea_codigo decimal(18,0),
 		vuelo_ruta_aerea_ciu_origen nvarchar(255),
 		vuelo_ruta_aerea_ciu_destino nvarchar(255),
-		vuelo_aerolinea_codigo nvarchar(255)
+		vuelo_aerolinea_codigo INT
 )
 
 CREATE TABLE LOS_BORBOTONES.CIUDAD (
-		ciudad_codigo nvarchar(255),
+		ciudad_codigo INT IDENTITY(1,1) NOT NULL,
 		ciudad_detalle nvarchar(255)
 )
 
 CREATE TABLE LOS_BORBOTONES.RUTA_AEREA (
-		ruta_aerea_codigo decimal(18,0),
-		ruta_aerea_ciu_origen nvarchar(255),
-		ruta_aerea_ciu_destino nvarchar(255)
+		ruta_aerea_codigo decimal(18,0) NOT NULL,
+		ruta_aerea_ciu_origen INT NOT NULL,
+		ruta_aerea_ciu_destino INT NOT NULL
 )
 
 CREATE TABLE LOS_BORBOTONES.TIPO_BUTACA (
-		tipo_butaca_codigo nvarchar(50),
+		tipo_butaca_codigo nvarchar(50) NOT NULL,
 		tipo_butaca_detalle nvarchar(50)
 )
 
 CREATE TABLE LOS_BORBOTONES.BUTACA (
-		butaca_id nvarchar(255),
+		butaca_id nvarchar(255) NOT NULL,
 		butaca_numero decimal(18,0),
 		butaca_tipo_butaca_codigo nvarchar(50),
 		butaca_avion_id nvarchar(50)
 )
 
 CREATE TABLE LOS_BORBOTONES.GRUPO_HOTELARIO (
-		grupo_hotelario_codigo nvarchar(255),
+		grupo_hotelario_codigo INT IDENTITY(1,1) NOT NULL,
 		grupo_hotelario_razon_social nvarchar(255)
 )
 
 CREATE TABLE LOS_BORBOTONES.HOTEL (
-		hotel_codigo nvarchar(255),
+		hotel_codigo nvarchar(255) NOT NULL,
 		hotel_calle nvarchar(50),
 		hotel_nro_calle decimal(18,0),
 		hotel_cantidad_estrellas decimal(18,0),
-		hotel_grupo_hotelario_codigo nvarchar(255)
+		hotel_grupo_hotelario_codigo INT
 )
 
 CREATE TABLE LOS_BORBOTONES.TIPO_HABITACION (
-		tipo_habitacion_codigo decimal(18,0),
+		tipo_habitacion_codigo decimal(18,0) NOT NULL,
 		tipo_habitacion_detalle nvarchar(255)
 )
 
 CREATE TABLE LOS_BORBOTONES.HABITACION (
-		habitacion_numero decimal(18,0),
-		habitacion_codigo_hotel nvarchar(255),
+		habitacion_numero decimal(18,0) NOT NULL,
+		habitacion_codigo_hotel nvarchar(255) NOT NULL,
 		habitacion_piso decimal(18,0),
 		habitacion_frente nvarchar(50),
 		habitacion_costo decimal(18,2),
@@ -154,18 +154,18 @@ CREATE TABLE LOS_BORBOTONES.HABITACION (
 
 CREATE TABLE LOS_BORBOTONES.FACTURA (
 		factura_fecha datetime2(3),
-		factura_numero decimal(18,0),
+		factura_numero decimal(18,0) NOT NULL,
 		factura_cliente_id int,
 		factura_sucursal_id decimal(18,0)
 )
 
 CREATE TABLE LOS_BORBOTONES.COMPRA_EMPRESA_TURISMO (
-		compra_empr_numero decimal(18,0),
+		compra_empr_numero decimal(18,0) NOT NULL,
 		compra_empr_fecha datetime2(3)
 )
 
 CREATE TABLE LOS_BORBOTONES.PASAJE (
-		pasaje_codigo decimal(18,0),
+		pasaje_codigo decimal(18,0) NOT NULL,
 		pasaje_costo decimal(18,2),
 		pasaje_precio decimal(18,2),
 		pasaje_vuelo_codigo decimal(18,0),
@@ -175,7 +175,7 @@ CREATE TABLE LOS_BORBOTONES.PASAJE (
 )
 
 CREATE TABLE LOS_BORBOTONES.ESTADIA (
-		estadia_codigo decimal(18,0),
+		estadia_codigo decimal(18,0) NOT NULL,
 		estadia_fecha_inicial datetime2(3),
 		estadia_cantidad_noches decimal(18,0),
 		estadia_hotel_codigo nvarchar(255),
@@ -223,3 +223,59 @@ ALTER TABLE LOS_BORBOTONES.PASAJE ADD CONSTRAINT FK_PASAJE_COMPRA_NUMERO FOREIGN
 ALTER TABLE LOS_BORBOTONES.ESTADIA ADD CONSTRAINT FK_ESTADIA_HOTEL_HABITACION FOREIGN KEY (estadia_hotel_codigo, estadia_habitacion_numero) REFERENCES LOS_BORBOTONES.HABITACION(habitacion_codigo_hotel, habitacion_numero)
 ALTER TABLE LOS_BORBOTONES.ESTADIA ADD CONSTRAINT FK_ESTADIA_FACTURA_NUMERO FOREIGN KEY (estadia_factura_numero) REFERENCES LOS_BORBOTONES.FACTURA(factura_numero)
 ALTER TABLE LOS_BORBOTONES.ESTADIA ADD CONSTRAINT FK_ESTADIA_COMPRA_NUMERO FOREIGN KEY (estadia_compra_numero) REFERENCES LOS_BORBOTONES.COMPRA_EMPRESA_TURISMO(compra_empr_numero)
+GO
+
+/* MIGRACION */
+
+CREATE PROC LOS_BORBOTONES.migracion_insert_clientes AS
+BEGIN
+	INSERT INTO LOS_BORBOTONES.CLIENTE(cliente_apellido, cliente_nombre, cliente_dni, cliente_fecha_nac, cliente_mail, cliente_telefono)
+		SELECT	CLIENTE_APELLIDO, CLIENTE_NOMBRE, CLIENTE_DNI, CLIENTE_FECHA_NAC, CLIENTE_MAIL, CLIENTE_TELEFONO
+		FROM gd_esquema.Maestra
+		WHERE CLIENTE_DNI IS NOT NULL
+		GROUP BY CLIENTE_DNI, CLIENTE_APELLIDO, CLIENTE_NOMBRE, CLIENTE_DNI, CLIENTE_FECHA_NAC, CLIENTE_MAIL, CLIENTE_TELEFONO
+END
+GO
+
+CREATE PROC LOS_BORBOTONES.migracion_insert_sucursales AS
+BEGIN
+	INSERT INTO LOS_BORBOTONES.SUCURSAL(sucursal_direccion, sucursal_mail, sucursal_telefono)
+		SELECT SUCURSAL_DIR, SUCURSAL_MAIL, SUCURSAL_TELEFONO
+		FROM gd_esquema.Maestra
+		WHERE SUCURSAL_DIR IS NOT NULL
+		GROUP BY SUCURSAL_DIR, SUCURSAL_MAIL, SUCURSAL_TELEFONO
+		ORDER BY SUCURSAL_MAIL
+END
+GO
+
+CREATE PROC LOS_BORBOTONES.migracion_insert_aerolineas AS
+BEGIN
+	INSERT INTO LOS_BORBOTONES.AEROLINEA(aerolinea_razon_social)
+		SELECT EMPRESA_RAZON_SOCIAL
+		FROM gd_esquema.Maestra
+		WHERE PASAJE_CODIGO IS NOT NULL
+		GROUP BY EMPRESA_RAZON_SOCIAL
+		ORDER BY EMPRESA_RAZON_SOCIAL
+END
+GO
+
+CREATE PROC LOS_BORBOTONES.migracion_insert_ciudades AS
+BEGIN
+	INSERT INTO LOS_BORBOTONES.CIUDAD(ciudad_detalle)
+		SELECT DISTINCT RUTA_AEREA_CIU_ORIG
+		FROM gd_esquema.Maestra
+		WHERE RUTA_AEREA_CODIGO IS NOT NULL AND RUTA_AEREA_CIU_ORIG IS NOT NULL
+		ORDER BY RUTA_AEREA_CIU_ORIG
+END
+GO
+
+CREATE PROC LOS_BORBOTONES.migracion_insert_grupos_hotelarios AS
+BEGIN
+	INSERT INTO LOS_BORBOTONES.GRUPO_HOTELARIO(grupo_hotelario_razon_social)
+		SELECT EMPRESA_RAZON_SOCIAL
+		FROM gd_esquema.Maestra
+		WHERE ESTADIA_CODIGO IS NOT NULL
+		GROUP BY EMPRESA_RAZON_SOCIAL
+		ORDER BY EMPRESA_RAZON_SOCIAL
+END
+GO
